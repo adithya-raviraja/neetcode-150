1b162767455f07beaec612c09cf6124430891646
Adithya Raviraja <adithya.raviraj96@gmail.com>
2024-11-17 09:13:48 +0000

    Generate parenthesis

--- stacks/generate_parenthesis.py
+++ stacks/generate_parenthesis.py
@@ -7,7 +7,27 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+        stack = list()
+        res = list()
+
+        def traverse_array(open_count, close_count):
+            if open_count == close_count == n:
+                res.append("".join(stack))
+                return
+            
+            if open_count < n:
+                stack.append('(')
+                traverse_array(open_count+1, close_count)
+                stack.pop()
+
+            if close_count < open_count:
+                stack.append(')')
+                traverse_array(open_count, close_count+1)
+                stack.pop()
+
         
+        traverse_array(0, 0)
+        return res
 
 if __name__ == "__main__":
     print('TIME COMPLEXITY: ')
