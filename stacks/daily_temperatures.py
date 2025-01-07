dc59ddae3ec2a3e48bbdf51823ace40d413417b4
Adithya Raviraja <adithya.raviraj96@gmail.com>
2025-01-07 14:31:03 +0000

    daily temperature

--- stacks/daily_temperatures.py
+++ stacks/daily_temperatures.py
@@ -1,7 +1,21 @@
-# Question Link: 
+# Question Link: https://neetcode.io/problems/daily-temperatures
 from typing import *
 
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        temp_res = [0] * len(temperatures)
+        stack = [] # Temp, Index
+
+        for index, temp in enumerate(temperatures):
+            while stack and temp > stack[-1][0]:
+                stack_temp, stack_index = stack.pop()
+                temp_res[stack_index] = (index - stack_index)
+            stack.append([temp, index])
+
+        return temp_res
 
 if __name__ == "__main__":
-    print('TIME COMPLEXITY: ')
-    print('SPACE COMPLEXITY: ')
+    temperatures = [30,38,30,36,35,40,28]
+    output = Solution().dailyTemperatures(temperatures)
+    print('TIME COMPLEXITY: O(n)')
+    print('SPACE COMPLEXITY: O(n)')
