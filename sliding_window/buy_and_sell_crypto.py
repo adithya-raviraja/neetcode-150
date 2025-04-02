35a66e4c8bf3cbc0a6fdec138ca2cf0603efef87
Adithya Raviraja <adithya.raviraj96@gmail.com>
2025-04-02 09:39:42 +0530

    buy and see crypto

---
+++ sliding_window/buy_and_sell_crypto.py
@@ -0,0 +1,26 @@
+# Question Link: https://neetcode.io/problems/buy-and-sell-crypto
+from typing import *
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l = 0
+        r = 0
+        maxP = 0
+        
+        for r in range(1, len(prices)):
+            if prices[r] > prices[l]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+        
+        return maxP
+                
+
+
+if __name__ == "__main__":
+    prices = [10,8,7,5,2]
+    output = Solution().maxProfit(prices)
+    print(output)
+    print('TIME COMPLEXITY: ')
+    print('SPACE COMPLEXITY: ')
